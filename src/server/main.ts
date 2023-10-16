21d01aaa7eae8555c729eb864f1886097e55d5ea
tatia2501 <catatat@yandex.ru>
2023-10-16 17:22:05 +0300

    fixing render

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,42 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { PORT } from 'src/shared/constants/env';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.initialize());
-
-  const config = new DocumentBuilder()
-    .setTitle('RiseOfCode')
-    .setDescription('The RiseOfCode App API description')
-    .setVersion('1.0')
-    .addTag('class')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
-  // app.setBaseViewsDir(join(__dirname, '..', 'views', 'pages'));
-  // app.setViewEngine('hbs');
-
-  // app.useGlobalPipes(new ValidationPipe());
-  //
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // const { httpAdapter } = app.get(HttpAdapterHost);
-  // app.useGlobalFilters(
-  //   new PrismaClientExceptionFilter(httpAdapter, {
-  //     P2000: HttpStatus.BAD_REQUEST,
-  //     P2025: HttpStatus.NOT_FOUND,
-  //   }),
-  // );
-
-  // hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
-  Logger.log(`Application is running on: http://localhost:3000`);
-  Logger.log(`Swagger is running on: http://localhost:3000/api`);
-
   await app.listen(PORT);
 
   if (module.hot) {
