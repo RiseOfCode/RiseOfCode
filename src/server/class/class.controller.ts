c29fc2233540c07b931825ddc274b1999d3d25f1
tatia2501 <catatat@yandex.ru>
2023-10-16 18:16:45 +0300

    fixing render

--- src/server/class/class.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ClassService } from './class.service';
-import { ApiTags } from '@nestjs/swagger';
-@ApiTags('class')
-@Controller('class')
-export class ClassController {
-  constructor(private classService: ClassService) {}
-}
