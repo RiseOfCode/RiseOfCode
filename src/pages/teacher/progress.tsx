94ead02adc9c7cf280215308d87fdb883b0daa53
tatia2501 <catatat@yandex.ru>
2023-11-13 00:05:50 +0300

    dropdown in teacher progress page

--- src/pages/teacher/progress.tsx
+++ src/pages/teacher/progress.tsx
@@ -4,16 +4,35 @@ import Link from 'next/link';
 const Classes = () => {
   const constClassId = '7046f5c8-7291-11ee-b962-0242ac120002';
 
-  // const [progress, setProgress] = useState([]);
-  //
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await fetch(`/api/progress/`)
-  //       .then((response) => response.json())
-  //       .then((data) => setProgress(data));
-  //   };
-  //   fetchData();
-  // }, []);
+  const [lessons, setLessons] = useState([]);
+
+  useEffect(() => {
+    const fetchLessonData = async () => {
+      await fetch(`/lesson/class/${constClassId}`)
+        .then((response) => response.json())
+        .then((data) =>
+          setLessons(
+            data.map((lesson) => (
+              <button onClick={(e: any) => handleLesson(lesson.id)}>
+                {lesson.name}
+              </button>
+            )),
+          ),
+        );
+    };
+    fetchLessonData();
+  }, []);
+  console.log(lessons);
+
+  const getProgress = async ({ lessonId }: { lessonId: string }) => {
+    await fetch(`/progress/teacher?lessonId=${lessonId}`).then((response) =>
+      response.json(),
+    ); // .then((data) => setTasks(data));
+  };
+
+  const handleLesson = (lessonId) => {
+    console.log(lessonId);
+  };
 
   return (
     <div className={styles.pageContainer}>
@@ -27,8 +46,39 @@ const Classes = () => {
         <Link href={`/teacher/progress`}>Прогресс</Link>
       </div>
       <div className={styles.main}>
-        {/*выпадной список с уроками*/}
+        <Dropdown
+          trigger={<button>Выбрать урок</button>}
+          AllLessons={lessons}
+        />
       </div>
+    </div>
+  );
+};
+
+const Dropdown = ({ trigger, AllLessons }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  return (
+    <div className="dropdown">
+      {React.cloneElement(trigger, {
+        onClick: handleOpen,
+      })}
+      {open ? (
+        <ul className="menu">
+          {AllLessons.map((lessonItem, index) => (
+            <li key={index} className="menu-item">
+              {React.cloneElement(lessonItem, {
+                onClick: () => {
+                  lessonItem.props.onClick();
+                  setOpen(false);
+                },
+              })}
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 };
