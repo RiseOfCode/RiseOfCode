13eeec902681aa63b948e9e421bb99283d2bf096
ann.komova <ann.komova@gmail.com>
2023-11-27 02:19:17 +0300

    fix prettier

--- src/pages/lessons/[id]/task/[taskId].tsx
+++ src/pages/lessons/[id]/task/[taskId].tsx
@@ -1,116 +1,117 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import styles from "../../../styles/task.module.css";
-import LocalHeader from "../../../../client/components/UI/Header";
-import StudentPages from "../../../student/Header";
+import styles from '../../../styles/task.module.css';
+import LocalHeader from '../../../../client/components/UI/Header';
+import StudentPages from '../../../student/Header';
 
 const TaskPage = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const userId = '42d59598-9548-41a3-bb42-d76635abb35c';
+  const userId = '42d59598-9548-41a3-bb42-d76635abb35c';
 
-    const { id, taskId } = router.query;
-    const classId = localStorage.getItem('classId')
+  const { id, taskId } = router.query;
+  const classId = localStorage.getItem('classId');
 
-    const [task, setTask] = useState({
-        id: '',
-        name: '',
-        description: ''
-    });
-    const [lessonData, setLessonData] = useState({
-        id: '',
-        name: ''
-    });
-    const [classData, setClassData] = useState({
-        name: '',
-        teacherInfo: '',
-        description: '',
-    });
+  const [task, setTask] = useState({
+    id: '',
+    name: '',
+    description: '',
+  });
+  const [lessonData, setLessonData] = useState({
+    id: '',
+    name: '',
+  });
+  const [classData, setClassData] = useState({
+    name: '',
+    teacherInfo: '',
+    description: '',
+  });
 
-    useEffect(() => {
-        const fetchClass = async () => {
-            try {
-                const response = await fetch(`/api/class/${classId}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setClassData(data);
-                } else {
-                    console.error('Failed to fetch class details');
-                }
-            } catch (error) {
-                console.error('An error occurred', error);
-            }
+  useEffect(() => {
+    const fetchClass = async () => {
+      try {
+        const response = await fetch(`/api/class/${classId}`);
+        if (response.ok) {
+          const data = await response.json();
+          setClassData(data);
+        } else {
+          console.error('Failed to fetch class details');
         }
-        const fetchLesson = async () => {
-            try {
-                const lessonResponse = await fetch(`/api/lesson/${id}`);
-                if (lessonResponse.ok) {
-                    const lessonData = await lessonResponse.json();
-                    setLessonData(lessonData);
-                } else {
-                    console.error('Failed to fetch lesson details');
-                }
+      } catch (error) {
+        console.error('An error occurred', error);
+      }
+    };
+    const fetchLesson = async () => {
+      try {
+        const lessonResponse = await fetch(`/api/lesson/${id}`);
+        if (lessonResponse.ok) {
+          const lessonData = await lessonResponse.json();
+          setLessonData(lessonData);
+        } else {
+          console.error('Failed to fetch lesson details');
+        }
 
-                const tasksResponse = await fetch(`/api/task/student/${userId}/${taskId}`);
-                if (tasksResponse.ok) {
-                    const tasksData = await tasksResponse.json();
-                    setTask(tasksData);
-                } else {
-                    console.error('Failed to fetch tasks');
-                }
-            } catch (error) {
-                console.error('An error occurred', error);
-            }
-        };
-
-        fetchClass();
-        fetchLesson();
-    }, [classId, id, taskId]);
-
-    const [textAnswer, setTextAnswer] = useState('');
-    const handleTextAnswerChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setTextAnswer(event.target.value);
+        const tasksResponse = await fetch(
+          `/api/task/student/${userId}/${taskId}`,
+        );
+        if (tasksResponse.ok) {
+          const tasksData = await tasksResponse.json();
+          setTask(tasksData);
+        } else {
+          console.error('Failed to fetch tasks');
+        }
+      } catch (error) {
+        console.error('An error occurred', error);
+      }
     };
 
-    const [file, setFile] = useState<File | null>(null);
+    fetchClass();
+    fetchLesson();
+  }, [classId, id, taskId]);
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFile = event.currentTarget.files?.[0] || null;
-        setFile(selectedFile);
-    };
+  const [textAnswer, setTextAnswer] = useState('');
+  const handleTextAnswerChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setTextAnswer(event.target.value);
+  };
 
-    const handleSubmit = async () => {
+  const [file, setFile] = useState<File | null>(null);
 
-    };
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.currentTarget.files?.[0] || null;
+    setFile(selectedFile);
+  };
 
-    return (
-        <div className={styles.container}>
-            <LocalHeader />
-            <h2>{classData ? classData.name : ""}</h2>
-            <StudentPages />
-            <h3 className={styles.lessonName}>{lessonData.name}</h3>
-            <h3 className={styles.taskName}>{task.name}</h3>
-            <p>{task.description}</p>
+  const handleSubmit = async () => {};
 
-            <textarea
-                className={`${styles.textarea} ${styles.button}`}
-                placeholder="Введите ваш ответ здесь..."
-                value={textAnswer}
-                onChange={handleTextAnswerChange}
-            />
+  return (
+    <div className={styles.container}>
+      <LocalHeader />
+      <h2>{classData ? classData.name : ''}</h2>
+      <StudentPages />
+      <h3 className={styles.lessonName}>{lessonData.name}</h3>
+      <h3 className={styles.taskName}>{task.name}</h3>
+      <p>{task.description}</p>
 
-            <input
-                className={`${styles.fileInput} ${styles.button}`}
-                type="file"
-                onChange={handleFileChange}
-            />
+      <textarea
+        className={`${styles.textarea} ${styles.button}`}
+        placeholder="Введите ваш ответ здесь..."
+        value={textAnswer}
+        onChange={handleTextAnswerChange}
+      />
 
-            <button className={styles.button} onClick={handleSubmit}>
-                Отправить
-            </button>
-        </div>
-    );
+      <input
+        className={`${styles.fileInput} ${styles.button}`}
+        type="file"
+        onChange={handleFileChange}
+      />
 
+      <button className={styles.button} onClick={handleSubmit}>
+        Отправить
+      </button>
+    </div>
+  );
 };
 
 export default TaskPage;
--- src/pages/student/Header.tsx
+++ src/pages/student/Header.tsx
@@ -3,22 +3,39 @@ import { useRouter } from 'next/router';
 import styles from '../styles/class-header.module.css';
 
 const StudentPages = () => {
+  const page = window.location.pathname;
 
-    let page = window.location.pathname
-
-    return (
-        <div>
-            <Link href="/student/description">
-                <a className={`${styles.navLink} ${page === '/student/description' ? styles.active : ''}`}>Описание</a>
-            </Link>
-            <Link href="/student/lessons">
-                <a className={`${styles.navLink} ${page === '/student/lessons' ? styles.active : ''}`}>Уроки</a>
-            </Link>
-            <Link href="/student/progress">
-                <a className={`${styles.navLink} ${page === '/student/progress' ? styles.active : ''}`}>Прогресс</a>
-            </Link>
-        </div>
-);
+  return (
+    <div>
+      <Link href="/student/description">
+        <a
+          className={`${styles.navLink} ${
+            page === '/student/description' ? styles.active : ''
+          }`}
+        >
+          Описание
+        </a>
+      </Link>
+      <Link href="/student/lessons">
+        <a
+          className={`${styles.navLink} ${
+            page === '/student/lessons' ? styles.active : ''
+          }`}
+        >
+          Уроки
+        </a>
+      </Link>
+      <Link href="/student/progress">
+        <a
+          className={`${styles.navLink} ${
+            page === '/student/progress' ? styles.active : ''
+          }`}
+        >
+          Прогресс
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default StudentPages;
