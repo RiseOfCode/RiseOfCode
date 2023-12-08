646a295f334722a8fa96a23a481fe82c52cd35bb
ann.komova <ann.komova@gmail.com>
2023-12-08 14:01:11 +0300

    merge main

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './styles/signin.module.css';
 import LocalHeader from '../client/components/UI/Header';
