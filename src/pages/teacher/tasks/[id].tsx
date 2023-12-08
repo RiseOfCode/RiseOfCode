5b79c6001feb375c268589ece5e534698f4b43cb
ann.komova <ann.komova@gmail.com>
2023-12-08 13:50:22 +0300

    add bank and task pages

---
+++ src/pages/teacher/tasks/[id].tsx
@@ -0,0 +1,98 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/task.module.css';
+import LocalHeader from '../../../client/components/UI/Header';
+
+const ThemesList = ({ themes }: { themes: string[] }) => {
+  return (
+    <div className={styles.classes}>
+      {themes.length > 0 ? (
+        themes.map((theme) => (
+          <div className={styles.classesPiece}>
+            <p>{theme}</p>
+          </div>
+        ))
+      ) : (
+        <p>В банке пока нет задач</p>
+      )}
+    </div>
+  );
+};
+
+const TaskPage = () => {
+  const router = useRouter();
+  //
+  // const userId = '42d59598-9548-41a3-bb42-d76635abb35c';
+
+  const { id } = router.query;
+
+  const [task, setTask] = useState({
+    id: '',
+    name: '',
+    themes: [],
+    rating: 0,
+    description: '',
+    tests: [{ input: '', output: '' }],
+  });
+
+  const fetchTask = async () => {
+    try {
+      const taskResponse = await fetch(`/api/task/teacher/${id}`);
+      if (taskResponse.ok) {
+        const taskData = await taskResponse.json();
+        setTask(taskData);
+      } else {
+        console.error('Failed to fetch tasks');
+      }
+    } catch (error) {
+      console.error('An error occurred', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTask();
+  });
+
+  const renderTestsTable = () => {
+    if (!task.tests || task.tests.length === 0) {
+      return <p>No tests.</p>;
+    }
+
+    return (
+      <table className={styles.attemptsTable}>
+        <thead>
+          <tr>
+            <th>N</th>
+            <th>Входные данные</th>
+            <th>Выходные данные</th>
+          </tr>
+        </thead>
+        <tbody>
+          {task.tests.map((test, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{test.input}</td>
+              <td>{test.output}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <LocalHeader />
+      <h3 className={styles.taskName}>{task.name}</h3>
+      <p>{task.rating}</p>
+      <div>
+        <ThemesList themes={task.themes} />
+      </div>
+      <p>{task.description}</p>
+
+      {renderTestsTable()}
+    </div>
+  );
+};
+
+export default TaskPage;
--- src/pages/teacher/tasks/bank.tsx
+++ src/pages/teacher/tasks/bank.tsx
@@ -12,18 +12,18 @@ const TasksList = ({
     id: string;
   }[];
 }) => {
-  // const router = useRouter();
-  //
-  // const handleBankClick = (id: string) => {
-  //   router.push(`teacher/tasks/${id}`);
-  // };
+  const router = useRouter();
+
+  const handleTaskClick = (id: string) => {
+    router.push(`/teacher/tasks/${id}`);
+  };
 
   return (
     <div className={styles.classes}>
       {tasks.length > 0 ? (
         tasks.map((task) => (
           <div className={styles.classesPiece} key={task.id}>
-            <p>{task.name}</p>
+            <p onClick={() => handleTaskClick(task.id)}>{task.name}</p>
             <p>{task.rating}</p>
           </div>
         ))
@@ -33,6 +33,7 @@ const TasksList = ({
     </div>
   );
 };
+
 const BankPage = () => {
   const [tasks, setTasks] = useState([
     {
@@ -43,33 +44,81 @@ const BankPage = () => {
   ]);
 
   const [filter, setFilter] = useState({
-    themes: null,
+    themes: undefined,
     minRating: 0,
-    isDesc: true,
+    isDesc: 'true',
   });
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        const tasksResponse = await fetch(`/api/task/bank`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(filter),
-        });
-        if (tasksResponse.ok) {
-          const tasksData = await tasksResponse.json();
-          setTasks(tasksData);
-        } else {
-          console.error('Failed to fetch tasks');
-        }
-      } catch (error) {
-        console.error('An error occurred', error);
-      }
+  const fetchTasks = async () => {
+    const data = {
+      themes: filter.themes,
+      minRating: filter.minRating,
+      isDesc: true,
     };
+    if (filter.isDesc == 'false') data.isDesc = false;
+    try {
+      const tasksResponse = await fetch(`/api/task/bank`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (tasksResponse.ok) {
+        const tasksData = await tasksResponse.json();
+        setTasks(tasksData);
+      } else {
+        console.error('Failed to fetch tasks');
+      }
+    } catch (error) {
+      console.error('An error occurred', error);
+    }
+  };
+
+  useEffect(() => {
     fetchTasks();
   });
+
+  const handleFilterThemes = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const fieldName = e.target.name;
+    const fieldValue = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value,
+    );
+
+    setFilter((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const handleFilterRating = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFilter((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const handleFilterDesc = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFilter((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const handleClear = () => {
+    setFilter({
+      themes: undefined,
+      minRating: 0,
+      isDesc: 'true',
+    });
+  };
 
   if (!tasks) {
     return <div>Loading...</div>;
@@ -78,6 +127,36 @@ const BankPage = () => {
   return (
     <div className={styles.pageContainer}>
       <LocalHeader />
+      <label>
+        Темы:
+        <select
+          value={filter.themes}
+          name="themes"
+          multiple={true}
+          onChange={handleFilterThemes}
+        >
+          <option value="BASE">Базовые операции</option>
+          <option value="ARITHMETIC">Арифметика</option>
+          <option value="LOGICS">Логические операторы</option>
+          <option value="IF">Условия</option>
+        </select>
+      </label>
+      <label>Минимальный рейтинг:</label>
+      <input
+        placeholder="0"
+        type="text"
+        value={filter.minRating}
+        name="minRating"
+        onChange={handleFilterRating}
+      />
+      <label>
+        Рейтинг:
+        <select value={filter.isDesc} name="isDesc" onChange={handleFilterDesc}>
+          <option value="true">по уменьшению</option>
+          <option value="false">по возрастанию</option>
+        </select>
+      </label>
+      <button onClick={handleClear}>Сбросить</button>
       <div>
         <h3>Банк задач</h3>
         <TasksList tasks={tasks} />
