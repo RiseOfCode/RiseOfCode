aa53191be385f1c2ae5491ebfcd45013c2f77873
Maria Teterina <teterina_m_o@rambler.ru>
2023-12-11 15:22:23 +0300

    add students front

--- src/pages/teacher/students.tsx
+++ src/pages/teacher/students.tsx
@@ -6,21 +6,42 @@ import React, { useEffect, useState } from 'react';
 import ClipboardJS from 'clipboard';
 import copy from './src/copy.png';
 import descStyles from '../styles/description.module.css';
-import { width } from '@material-ui/system';
+import addUserPng from './src/addUser.png';
 
 const Students = () => {
   const [classInfo, setClassInfo] = useState({
+    id: '',
     name: '',
     code: '',
   });
+  const [students, setStudents] = useState([
+    {
+      name: '',
+      surname: '',
+      nickname: '',
+    },
+  ]);
+  const [newUserName, setNewUserName] = useState('');
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const constClassId = localStorage.getItem('classId') ?? '';
     const fetchData = async () => {
-      await fetch(`/api/class/${constClassId}`)
-        .then((response) => response.json())
-        .then((data) => setClassInfo(data));
+      try {
+        const responseClass = await fetch(`/api/class/${constClassId}`);
+        const dataClass = await responseClass.json();
+        setClassInfo(dataClass);
+
+        const responseStudents = await fetch(
+          `/api/class/${constClassId}/students`,
+        );
+        const dataStudents = await responseStudents.json();
+        setStudents(dataStudents);
+      } catch (error) {
+        console.error('An error occurred while fetching data:', error);
+      }
     };
+
     fetchData();
   }, []);
 
@@ -42,8 +63,31 @@ const Students = () => {
     };
   }, []);
 
-  const addUser = (e: any) => {
-    // (e.target.value);
+  const addUser = async (e: any) => {
+    setNewUserName(e.target.value);
+    setError(null); // Сброс ошибки при изменении значения ввода
+  };
+
+  const handleAddUser = async () => {
+    try {
+      const response = await fetch(
+        `/api/class/add/nickname?nickname=${newUserName}&classId=${classInfo.id}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        setError('Пользователь с таким никнеймом не найден');
+      }
+    } catch (error) {
+      setError('Произошла ошибка при добавлении пользователя');
+    }
   };
 
   return (
@@ -77,6 +121,30 @@ const Students = () => {
           placeholder="Новый студент"
           onChange={(e: any) => addUser(e)}
         />
+        <img
+          src={addUserPng.src}
+          width="25"
+          height="25"
+          style={{ marginTop: '10px', cursor: 'pointer' }}
+          onClick={(e: any) => handleAddUser()}
+        ></img>
+        {error && (
+          <p className={styles.error} style={{ color: 'red' }}>
+            {error}
+          </p>
+        )}
+      </div>
+      <div>
+        <h4 className={styles.mainName}>Список студентов:</h4>
+        <ul className={styles.student}>
+          {students.map((student, index) => (
+            <li key={index}>
+              <p className={styles.item}>{student.nickname}</p>
+              <p className={styles.item}>{student.name}</p>
+              <p className={styles.item}>{student.surname}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
